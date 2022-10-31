53c76a4a94f95c6573481aa5df868fdfb53192f1
Nolasco <nolasco.jimenez@solera.com>
2022-10-31 16:53:10 +0100

    findAvailableFlights (WIP)

--- soleraAirlines/src/main/java/com/grupoRocodromo/soleraAirlines/soleraAirlines/Service/FlightService.java
+++ soleraAirlines/src/main/java/com/grupoRocodromo/soleraAirlines/soleraAirlines/Service/FlightService.java
@@ -1,17 +1,34 @@
 package com.grupoRocodromo.soleraAirlines.soleraAirlines.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.grupoRocodromo.soleraAirlines.soleraAirlines.model.City;
 import com.grupoRocodromo.soleraAirlines.soleraAirlines.model.Flight;
 
 public interface FlightService {
 
-	List<Flight> findAllFlights();
-
 	Flight createFlight(Flight flights);
+	
+	List<Flight> findAllFlights();
+	
+	Flight updateFlight(Flight flights);
 
 	boolean deleteFlight(Long id);
+	
+	// Looks for all the available flights with the selected departure city
+	public static List<Flight> findAvailableFlights(String departureCity){
+		
+		List<Flight> flights.findAllFlights();
+		
+		for (Flight flight :flights) {
+			City city = flight.getDepartureCity();
+			if (city.getName() == departureCity) {
+				flights.remove(flights.indexOf(flights));
+			}
+		}
 
-	Flight updateFlight(Flight flights);
+		return flights;
+	};
 
 }
--- soleraAirlines/src/main/java/com/grupoRocodromo/soleraAirlines/soleraAirlines/Service/impl/FlightServiceImpl.java
+++ soleraAirlines/src/main/java/com/grupoRocodromo/soleraAirlines/soleraAirlines/Service/impl/FlightServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.grupoRocodromo.soleraAirlines.soleraAirlines.Service.FlightService;
+import com.grupoRocodromo.soleraAirlines.soleraAirlines.model.City;
 import com.grupoRocodromo.soleraAirlines.soleraAirlines.model.Flight;
 import com.grupoRocodromo.soleraAirlines.soleraAirlines.repository.FlightRepository;
 
@@ -16,15 +17,20 @@ public class FlightServiceImpl implements FlightService {
 	private FlightRepository flightRepository;
 
 	@Override
+	public Flight createFlight(Flight flights) {
+		return flightRepository.save(flights);
+	}
+	
+	@Override
 	public List<Flight> findAllFlights() {
 		return flightRepository.findAll();
 	}
 
 	@Override
-	public Flight createFlight(Flight flights) {
+	public Flight updateFlight(Flight flights) {
 		return flightRepository.save(flights);
 	}
-
+	
 	@Override
 	public boolean deleteFlight(Long id) {
 		if (flightRepository.existsById(id)) {
@@ -34,10 +40,4 @@ public class FlightServiceImpl implements FlightService {
 			return false;
 		}
 	}
-
-	@Override
-	public Flight updateFlight(Flight flights) {
-		return flightRepository.save(flights);
-	}
-
 }
