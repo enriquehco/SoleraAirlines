1ccbdb11a572824e20649b32d43f841f41c8d0fd
Estebanst99 <estebanbena20@gmail.com>
2022-11-03 10:18:00 +0100

    First Tests

--- microservices/flight-service/src/test/java/com/esteban/flightservice/FlightServiceApplicationTests.java
+++ microservices/flight-service/src/test/java/com/esteban/flightservice/FlightServiceApplicationTests.java
@@ -1,13 +1,63 @@
 package com.esteban.flightservice;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.esteban.flightservice.service.impl.FlightServiceImpl;
 
 @SpringBootTest
 class FlightServiceApplicationTests {
 
+	Double basePrice;
+	int age;
+	boolean luggage = true;
+
+	private FlightServiceImpl flightServiceImpl = new FlightServiceImpl();
+
 	@Test
-	void contextLoads() {
+	void setPricePerUser_WhenAgeLessThan2_ReturnZero() {
+
+		Double expected = 0.0;
+		basePrice = 10.2;
+		age = 1;
+		luggage = false;
+		
+		Double result = flightServiceImpl.setPricePerUser(basePrice, age, luggage);
+		
+		assertEquals(expected, result);
 	}
+	
+	@Test
+	void setPricePerUser_WhenAgeBetweenTwoAndNine_ReturnPriceWithDiscount0_5() {
+
+		Double expected = 5.0;
+		basePrice = 10.0;
+		age = 3;
+		luggage = false;
+		
+		Double result = flightServiceImpl.setPricePerUser(basePrice, age, luggage);
+		
+		assertEquals(expected, result);
+	}
+	
+	
+	@Test
+	void setPricePerUser_WhenAgeMoreThanNine_ReturnPriceWithDiscount1() {
+
+		Double expected = 10.0;
+		basePrice = 10.0;
+		age = 18;
+		luggage = false;
+		
+		Double result = flightServiceImpl.setPricePerUser(basePrice, age, luggage);
+		
+		assertEquals(expected, result);
+	}
+	
 
 }
--- microservices/purchase-service/src/test/java/com/esteban/purchaseservice/PurchaseServiceApplicationTests.java
+++ microservices/purchase-service/src/test/java/com/esteban/purchaseservice/PurchaseServiceApplicationTests.java
@@ -1,13 +1,37 @@
 package com.esteban.purchaseservice;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.esteban.purchaseservice.service.impl.PurchaseServiceImpl;
 
 @SpringBootTest
 class PurchaseServiceApplicationTests {
 
+	private PurchaseServiceImpl purchaseServiceImpl = new PurchaseServiceImpl();
+
 	@Test
-	void contextLoads() {
+	void completedPurchase_WhenSurnameIsError_ReturnFalse() {
+
+		String surname = "error";
+
+		boolean result = purchaseServiceImpl.purchaseCompleted(surname);
+
+		assertEquals(result, false);
+
+	}
+
+	@Test
+	void completedPurchase_WhenSurnameIsNotError_ReturnTrue() {
+
+		String surname = "hola";
+
+		boolean result = purchaseServiceImpl.purchaseCompleted(surname);
+
+		assertEquals(result, true);
+
 	}
 
 }
